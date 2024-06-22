a22130da750b5687d8f3998b6c296703a2fd590d
CodingCookiee <rzqutubawan@gmail.com>
2024-06-22 07:41:24 +0000

    fixed minor issues

--- Calculator/Calculator.js
+++ Calculator/Calculator.js
@@ -6,20 +6,33 @@ document.addEventListener("DOMContentLoaded", () => {
   const clear = document.getElementById("clear");
   const backspace = document.getElementById("backspace");
   const sign = document.getElementById("sign");
+
   let currentNumber = "";
   let previousNumber = "";
-  let operator = "";
+  let operatorValue = "";
   let resultValue = "";
-  numbers
 
-  for (const operator of operators) {
+  numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+      if (resultValue) {
+        resultValue = "";
+        result.textContent = "";
+      }
+      currentNumber += number.textContent;
+      result.textContent = currentNumber;
+    });
+  });
+
+  operators.forEach((operator) => {
     operator.addEventListener("click", () => {
+      if (currentNumber === "") return; // Prevents setting operator if currentNumber is empty
       previousNumber = currentNumber;
       currentNumber = "";
       operatorValue = operator.textContent;
       result.textContent = previousNumber + " " + operatorValue;
     });
-  }
+  });
+
   equals.addEventListener("click", () => {
     if (previousNumber && currentNumber && operatorValue) {
       resultValue = calculate(previousNumber, currentNumber, operatorValue);
@@ -29,6 +42,7 @@ document.addEventListener("DOMContentLoaded", () => {
       operatorValue = "";
     }
   });
+
   clear.addEventListener("click", () => {
     resultValue = "";
     previousNumber = "";
@@ -36,14 +50,21 @@ document.addEventListener("DOMContentLoaded", () => {
     operatorValue = "";
     result.textContent = "";
   });
+
   backspace.addEventListener("click", () => {
-    currentNumber = currentNumber.slice(0, -1);
-    result.textContent = currentNumber;
+    if (currentNumber) {
+      currentNumber = currentNumber.slice(0, -1);
+      result.textContent = currentNumber;
+    }
   });
+
   sign.addEventListener("click", () => {
-    currentNumber = -currentNumber;
-    result.textContent = currentNumber;
+    if (currentNumber) {
+      currentNumber = (parseFloat(currentNumber) * -1).toString();
+      result.textContent = currentNumber;
+    }
   });
+
   function calculate(previousNumber, currentNumber, operatorValue) {
     let result = 0;
     switch (operatorValue) {
